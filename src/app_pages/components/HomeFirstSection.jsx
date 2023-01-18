efbe34073f38e58723827f25b7c561d1814dd6ae
PacoNavarrete <102440729+PacoNavarrete@users.noreply.github.com>
2023-01-17 20:13:46 -0800

    3 files modified, 1 added[testing xstyled]

---
+++ src/app_pages/components/HomeFirstSection.jsx
@@ -0,0 +1,14 @@
+import {x} from '@xstyled/styled-components':
+
+export const HomeFirstSection = () => {
+
+  return (
+    <x.div
+      display="grid"
+      gridTemplateColumns={3}
+      gridTemplateAreas=""
+    >
+
+    </x.div>
+  )
+}
--- src/app_pages/pages/HomePage.jsx
+++ src/app_pages/pages/HomePage.jsx
@@ -3,12 +3,35 @@ import { HomeCTASection } from '../components/HomeCTASection';
 import { HomeNav } from '../../navigation/header/HomeNav';
 import { FlexBox } from '../../styled_components/StyledContainers';
 
+import { x } from '@xstyled/styled-components';
+
 export const HomePage = () => {
   return (
-    <FlexBox flexFlow="column nowrap" minHeight="100vh">
-      <HomeNav />
-      <HomeHeroSection />
-      <HomeCTASection />
-    </FlexBox>
+    <x.div
+      minH="100vh"
+      w="90%"
+      m="0 auto"
+      display="grid"
+      gridTemplateColumns={3}
+      gridTemplateAreas={{ xl: '"a a a" "b b c"', md: '"c c b" "c c a"' }}
+    >
+      <x.div gridArea="a" bg="#00ffff">
+        A
+      </x.div>
+      <x.div gridArea="b" bg="#ff00ff">
+        B
+      </x.div>
+      <x.div gridArea="c" bg="#ff0000">
+        C
+      </x.div>
+    </x.div>
   );
+
+  // return (
+  //   <FlexBox flexFlow="column nowrap" minHeight="100vh">
+  //     <HomeNav />
+  //     <HomeHeroSection />
+  //     <HomeCTASection />
+  //   </FlexBox>
+  // );
 };
