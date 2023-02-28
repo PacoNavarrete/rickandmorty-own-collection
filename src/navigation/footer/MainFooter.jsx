683aacddce70f939969d8a3ce513b4f00431925b
PacoNavarrete <102440729+PacoNavarrete@users.noreply.github.com>
2023-02-27 23:02:57 -0800

    Make: Refactoring MainFooter for better readability

--- src/navigation/footer/MainFooter.jsx
+++ src/navigation/footer/MainFooter.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { FlexBox } from '../../styled_components/StyledContainers';
 import { UListBox } from '../../styled_components/StyledNavigation';
 import {
@@ -8,6 +7,52 @@ import {
   TextXTiny,
 } from '../../styled_components/StyledText';
 import { Divider } from '../../styled_components/StyledUtils';
+
+const ContactList = () => {
+  return (
+    <UListBox color="#fff" width="322px" gridArea="socials">
+      <li>
+        <a href="https://github.com/PacoNavarrete" target="_blank">
+          <TextXTiny cursor="pointer">Github</TextXTiny>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/francisco-cienfuegos-291619164/"
+          target="_blank"
+        >
+          <TextXTiny cursor="pointer">LinkedIn</TextXTiny>
+        </a>
+      </li>
+    </UListBox>
+  );
+};
+
+const ContactImages = () => {
+  return (
+    <FlexBox gridArea="icons" gap="10px">
+      <a href="https://github.com/PacoNavarrete" target="_blank">
+        <img
+          src="https://res.cloudinary.com/paconavarrete/image/upload/v1674247926/RickandMorty/github-logo_dokku0.png"
+          width="28px"
+          height="28px"
+          alt="github icon"
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/francisco-cienfuegos-291619164/"
+        target="_blank"
+      >
+        <img
+          src="https://res.cloudinary.com/paconavarrete/image/upload/v1674248406/RickandMorty/linkedin_ja7eg5.png"
+          width="28px"
+          height="28px"
+          alt="linkedin icon"
+        />
+      </a>
+    </FlexBox>
+  );
+};
 
 export const MainFooter = () => {
   return (
@@ -19,40 +64,16 @@ export const MainFooter = () => {
         <TextMedium margin="auto 0" gridArea="contact">
           Get in touch
         </TextMedium>
-        <UListBox color="#fff" width="322px" gridArea="socials">
-          <li>
-            <Link to="https://github.com/PacoNavarrete">
-              <TextXTiny cursor="pointer">Github</TextXTiny>
-            </Link>
-          </li>
-          <li>
-            <Link to="https://www.linkedin.com/in/francisco-cienfuegos-291619164/">
-              <TextXTiny cursor="pointer">LinkedIn</TextXTiny>
-            </Link>
-          </li>
-        </UListBox>
+        <ContactList />
         <Divider
           gridArea="divider"
           width="100%"
           height="0.5px"
           backgroundColor="#F2F2F2"
         />
-        <FlexBox gridArea="icons" gap="10px">
-          <img
-            src="https://res.cloudinary.com/paconavarrete/image/upload/v1674247926/RickandMorty/github-logo_dokku0.png"
-            width="28px"
-            height="28px"
-            alt="github icon"
-          />
-          <img
-            src="https://res.cloudinary.com/paconavarrete/image/upload/v1674248406/RickandMorty/linkedin_ja7eg5.png"
-            width="28px"
-            height="28px"
-            alt="linkedin icon"
-          />
-        </FlexBox>
+        <ContactImages />
         <TextXSmall gridArea="madeby">
-          &lt; developed /&gt; by Paco Navarrete
+          Hand Crafted by Paco Navarrete
         </TextXSmall>
       </div>
     </>
